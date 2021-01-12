0df6507bfa07a0d9b61643f5824fbdd7e736965a
Carter Venne <you@example.com>
2021-01-12 16:36:22 -0600

    some clean  up/comments

--- assets/logic.js
+++ assets/logic.js
@@ -6,9 +6,7 @@ var timerId;
 var questionsEl = document.getElementById("questions");
 var timerEl = document.getElementById("time");
 var choicesEl = document.getElementById("choices");
-var submitBtn = document.getElementById("submit");
 var startBtn = document.getElementById("start");
-// var initialsEl = document.getElementById("initials");
 
 // timer countdown function;
 function clockTick() {
