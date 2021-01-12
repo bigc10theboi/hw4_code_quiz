2673d6f7e32182d2e4e2e4490be24eb28a03fd17
Carter Venne <you@example.com>
2021-01-12 15:10:04 -0600

    added template questions

---
+++ assets/questions.js
@@ -0,0 +1,27 @@
+let questions = [
+    {
+      title: "Who invented JavaScript?",
+      choices: ["Douglas Crockford", "Sheryl Sandberg", "Brendan Eich"],
+      answer: "Brendan Eich",
+    },
+    {
+      title: "Which one of these is a JavaScript package manager?",
+      choices: ["Node.js", "TypeScript", "npm"],
+      answer: "npm",
+    },
+    {
+      title: "Which tool can you use to ensure code quality?",
+      choices: ["Angular", "jQuery", "RequireJS", "ESLint"],
+      answer: "ESLint",
+    },
+    {
+      title: "Which symbol is used for comments in Javascript?",
+      choices: ["//", "/*", "Line", "Comment"],
+      answer: "//",
+    },
+    {
+      title: "What are all the looping structures in Javascript?",
+      choices: ["For", "While", "do-while loops"],
+      answer: "For",
+    },
+  ];
